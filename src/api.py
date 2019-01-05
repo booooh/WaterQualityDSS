76c350b8eddee12489bcef256995c797a98c98ef
Ben Dayan <booooh@users.noreply.github.com>
2019-01-05 12:19:01 +0200

    feat: Add functionality for uploading a file and accessing parameters (#2)
    * Create api_test.py
    
    * Update Dockerfile
    
    * Added unit tests to docker file
    
    * multiple build stages in Dockerfile
    
    * Adding travis pipeline
    
    * update deploy section in travis.yml
    
    * Adding file upload and background processing

--- src/api.py
+++ src/api.py
@@ -1,10 +1,31 @@
 import responder
+import processing
 
 api = responder.API()
 
-@api.route("/")
-async def hello(req, resp):
-    resp.text = "hello, world!"
+@api.route("/status/{exec_id}")
+async def status(req, resp, * , exec_id):
+    if exec_id in processing.EXECUTIONS:
+        resp.media = {"id" : exec_id, "status": processing.EXECUTIONS[exec_id]}
+    else:
+        resp.media = {"id" : exec_id, "status": "NOT_FOUND"}
+
+@api.route("/model")
+async def exec_model(req, resp):
+    """
+    Get the uploaded file, execute the model in the background
+    """
+    print("going to await data")
+    params = await req.media("files")
+    print(f"got {params.keys()}")    
+    exec_id = processing.get_exec_id()
+    
+    @api.background.task
+    def task():
+        processing.execute_model(exec_id, params)
+    
+    task()
+    resp.media = {"id" : exec_id}
 
 if __name__ == "__main__":
     api.run()
--- test/api_test.py
+++ test/api_test.py
@@ -1,11 +1,31 @@
+import time
+
 import pytest
+
 import api as service
+import processing
 
 @pytest.fixture
 def api():
     return service.api
 
+def test_execution_not_found(api):
+    assert api.requests.get(f"/status/DOES_NOT_EXIST").json()['status'] == 'NOT_FOUND'
 
-def test_hello_world(api):
-    r = api.requests.get("/")
-    assert r.text == "hello, world!"
+def test_model_execution(api, tmp_path):
+    file_obj = tmp_path / "data.t"
+    file_obj.write_bytes("this is some value".encode())
+    PROCESSING_DURATION = 3
+    
+    files = {'model': (file_obj.name, file_obj.read_bytes(), 'application/octet-stream')}
+    data = {'processing_duration' : PROCESSING_DURATION}
+    resp = api.requests.post("/model", files=files, data=data)    
+    model_response = resp.json()
+    assert 'id' in model_response
+    exec_id = model_response['id']
+
+    assert api.requests.get(f"/status/{exec_id}").json()['status'] == 'RUNNING'
+    time.sleep(PROCESSING_DURATION + 2)
+    assert api.requests.get(f"/status/{exec_id}").json()['status'] == 'COMPLETED'
+
+
