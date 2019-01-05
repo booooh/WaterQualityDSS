7e15dc05a3694b4db69d270a7871e19b392d4742
Booh <ben.dayan@gmail.com>
2019-01-05 16:35:00 +0200

    fix: renamed to dss processing, and used parameter values in api

--- test/api_test.py
+++ test/api_test.py
@@ -12,20 +12,24 @@ def api():
 def test_execution_not_found(api):
     assert api.requests.get(f"/status/DOES_NOT_EXIST").json()['status'] == 'NOT_FOUND'
 
-def test_model_execution(api, tmp_path):
+def test_dss_execution(api, tmp_path):
+    CONTENTS="this is some value"
+
     file_obj = tmp_path / "data.t"
-    file_obj.write_bytes("this is some value".encode())
-    PROCESSING_DURATION = 3
+    file_obj.write_bytes(CONTENTS.encode())
+    PROCESSING_DURATION = 1
     
     files = {'model': (file_obj.name, file_obj.read_bytes(), 'application/octet-stream')}
     data = {'processing_duration' : PROCESSING_DURATION}
-    resp = api.requests.post("/model", files=files, data=data)    
+    resp = api.requests.post("/dss", data=data, files=files)    
     model_response = resp.json()
     assert 'id' in model_response
     exec_id = model_response['id']
 
-    assert api.requests.get(f"/status/{exec_id}").json()['status'] == 'RUNNING'
-    time.sleep(PROCESSING_DURATION + 2)
-    assert api.requests.get(f"/status/{exec_id}").json()['status'] == 'COMPLETED'
+    assert api.requests.get(f"/status/{exec_id}").json()['status'] == processing.ExectuionState.RUNNING.value
+    time.sleep(PROCESSING_DURATION + 0.1)
+    resp = api.requests.get(f"/status/{exec_id}").json()    
+    assert resp['status'] == processing.ExectuionState.COMPLETED.value
+    assert resp['result'] == CONTENTS
 
 
